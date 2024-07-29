91a315774ec81d5ccff059526dfa696998decbb5
tcsimm <widet77@icloud.com>
2024-07-29 13:49:20 -0400

    fix

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
-import express from 'express';
-import Hello from './Hello.js';
-import Lab5 from './Lab5/index.js';
-
-const app = express();
-
-Hello(app);
-Lab5(app);
-
-app.listen(4000, () => {
-  console.log('Server is running on port 4000');
-});
+import express from 'express'
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5/index.js";
+const app = express()
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000)

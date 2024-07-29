4767c035ccece32dd996b6c986f2057cd825724b
tcsimm <widet77@icloud.com>
2024-07-29 09:44:41 -0400

    commit

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-import express from 'express'
-import Hello from "./Hello.js"
-const app = express()
-Hello(app)
-app.listen(4000)
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5/index.js';
 
+const app = express();
+app.use(express.json());
+
+Lab5(app);
+Hello(app);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- Hello.js
+++ Hello.js
@@ -1,10 +1,10 @@
 export default function Hello(app) {
     app.get('/hello', (req, res) => {
-      res.send('Life is good!')
-    })
-    app.get('/', (req, res) => {
-      res.send('Welcome to Full Stack Development!')
-    })
-  }
+      res.send('Life is good!');
+    });
   
+    app.get('/', (req, res) => {
+      res.send('Welcome to Full Stack Development!');
+    });
+  }
   

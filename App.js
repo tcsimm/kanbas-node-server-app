5a82f5fbee7f4ccf70b2c6d6f48874c5e97e601d
tcsimm <widet77@icloud.com>
2024-07-29 09:47:11 -0400

    fix

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import express from 'express';
-import Hello from './Hello.js';
-import Lab5 from './Lab5/index.js';
+import express from 'express'
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5/index.js";
+const app = express()
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000)
 
-const app = express();
-app.use(express.json());
-
-Lab5(app);
-Hello(app);
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});

8c1cfa7b6342d0f5391c25f3be6da396df772bd9
tcsimm <widet77@icloud.com>
2024-07-29 13:43:14 -0400

    more

--- App.js
+++ App.js
@@ -3,12 +3,10 @@ import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
 
 const app = express();
-app.use(express.json());
 
-Lab5(app);
 Hello(app);
+Lab5(app);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });

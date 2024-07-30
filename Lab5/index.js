21237d9c4c9dbd074a3f1c6e483dfb80433fa9cb
tcsimm <widet77@icloud.com>
2024-07-30 07:40:47 -0400

    7/30 start

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-
+import PathParameters from "./PathParameters.js";
 export default function Lab5(app) {
-  app.get('/lab5/welcome', (req, res) => {
-    res.send('Welcome to Lab 5');
+  app.get("/lab5/welcome", (req, res) => {
+    res.send("Welcome to Lab 5");
   });
+  PathParameters(app);
 }
- 

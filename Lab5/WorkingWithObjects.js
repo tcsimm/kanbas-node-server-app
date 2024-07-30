d2646062b5f3935b7174f86d7ffa6c18165bd047
tcsimm <widet77@icloud.com>
2024-07-30 08:14:45 -0400

    3.4.2

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -7,6 +7,13 @@ const assignment = {
     score: 0,
   };
   
+  const moduleObject = {
+    id: "1",
+    name: "Module 1",
+    description: "Introduction to NodeJS",
+    course: "CS5610",
+  };
+  
   export default function WorkingWithObjects(app) {
     app.get("/lab5/assignment", (req, res) => {
       res.json(assignment);
@@ -19,6 +26,40 @@ const assignment = {
     app.get("/lab5/assignment/title/:newTitle", (req, res) => {
       const { newTitle } = req.params;
       assignment.title = newTitle;
+      res.json(assignment);
+    });
+  
+    // Module routes
+    app.get("/lab5/module", (req, res) => {
+      res.json(moduleObject);
+    });
+  
+    app.get("/lab5/module/name", (req, res) => {
+      res.json(moduleObject.name);
+    });
+  
+    app.get("/lab5/module/name/:newName", (req, res) => {
+      const { newName } = req.params;
+      moduleObject.name = newName;
+      res.json(moduleObject);
+    });
+  
+    app.get("/lab5/module/description/:newDescription", (req, res) => {
+      const { newDescription } = req.params;
+      moduleObject.description = newDescription;
+      res.json(moduleObject);
+    });
+  
+    // Assignment properties modification
+    app.get("/lab5/assignment/score/:newScore", (req, res) => {
+      const { newScore } = req.params;
+      assignment.score = parseInt(newScore);
+      res.json(assignment);
+    });
+  
+    app.get("/lab5/assignment/completed/:status", (req, res) => {
+      const { status } = req.params;
+      assignment.completed = status === "true";
       res.json(assignment);
     });
   }
--- Lab5/index.js
+++ Lab5/index.js
@@ -1,6 +1,7 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
@@ -9,4 +10,5 @@ export default function Lab5(app) {
   PathParameters(app);
   QueryParameters(app);
   WorkingWithObjects(app);
+  WorkingWithArrays(app);
 }

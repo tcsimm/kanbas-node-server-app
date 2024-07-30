a39538453023906c603d6d9b79563d459c145f42
tcsimm <widet77@icloud.com>
2024-07-30 08:39:58 -0400

    3.4.5

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -34,5 +34,12 @@ let todos = [
       }
       res.json(todos);
     });
+  
+    app.get("/lab5/todos/:id/delete", (req, res) => {
+      const { id } = req.params;
+      const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+      todos.splice(todoIndex, 1);
+      res.json(todos);
+    });
   }
   

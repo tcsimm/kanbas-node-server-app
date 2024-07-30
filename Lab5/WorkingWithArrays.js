8be9b15a6974c58767bf20af1dd7f278f257b21e
tcsimm <widet77@icloud.com>
2024-07-30 14:26:04 -0400

    3.6.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -58,6 +58,17 @@ export default function WorkingWithArrays(app) {
       res.sendStatus(200);
   });
 
+  app.put("/lab5/todos/:id", (req, res) => {
+      const { id } = req.params;
+      todos = todos.map((t) => {
+          if (t.id === parseInt(id)) {
+              return { ...t, ...req.body };
+          }
+          return t;
+      });
+      res.sendStatus(200);
+  });
+
   app.get("/lab5/todos/:id/title/:title", (req, res) => {
       const { id, title } = req.params;
       const todo = todos.find((t) => t.id === parseInt(id));

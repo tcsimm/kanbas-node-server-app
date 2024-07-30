4eb59b78a59ecb68c1b563d0e767f39f0cd67fb1
tcsimm <widet77@icloud.com>
2024-07-30 14:38:06 -0400

    4.1

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,33 @@
+export default[
+  {
+    "_id": "module1",
+    "course": "RS101",
+    "name": "Introduction to Rocket Propulsion",
+    "lessons": [
+      { "name": "Basics of Rocket Propulsion" },
+      { "name": "Fuel Types and Chemistry" },
+      { "name": "Practical Applications" }
+    ]
+  },
+  {
+    "_id": "module2",
+    "course": "RS101",
+    "name": "Advanced Rocket Propulsion",
+    "lessons": [
+      { "name": "Advanced Fuel Chemistry" },
+      { "name": "Engine Design" },
+      { "name": "Performance Optimization" }
+    ]
+  },
+  {
+    "_id": "module3",
+    "course": "RS101",
+    "name": "Rocket Propulsion Lab",
+    "lessons": [
+      { "name": "Lab Safety" },
+      { "name": "Hands-On Experiments" },
+      { "name": "Data Analysis" }
+    ]
+  }
+
+]

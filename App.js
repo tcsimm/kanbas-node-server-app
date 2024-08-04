52196a2b85862e5235bfb3e9cd608d5c554a65c3
tcsimm <widet77@icloud.com>
2024-08-04 12:49:48 -0400

    push

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import Hello from "./Hello.js";
 
 const corsOptions = {
   origin: 'http://localhost:3000', 
@@ -17,6 +18,7 @@ CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
+Hello(app);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");

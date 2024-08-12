fae819d561b21eb86b3b4c338266e54c6533dd7a
tcsimm <widet77@icloud.com>
2024-08-12 16:30:31 -0400

    812 4

--- App.js
+++ App.js
@@ -9,13 +9,12 @@ import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import Hello from "./Hello.js";
 import UserRoutes from "./Users/routes.js";
 
-// MongoDB connection string
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 
-// Connect to MongoDB
 mongoose.connect(CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -26,33 +25,27 @@ mongoose.connect(CONNECTION_STRING, {
 
 const app = express();
 
-// CORS options
 const corsOptions = {
   origin: process.env.NETLIFY_URL || "http://localhost:3000",
   credentials: true, 
 };
 
-// Apply CORS middleware
 app.use(cors(corsOptions));
 
-// JSON middleware
 app.use(express.json());
 
-// Session options
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
   cookie: {
-    sameSite: "lax",  // Set to "lax" instead of "none" for development purposes
-    secure: process.env.NODE_ENV === "production",  // Ensure cookies are only sent over HTTPS in production
+    sameSite: "lax", 
+    secure: process.env.NODE_ENV === "production",
   },
 };
 
-// Apply session middleware
 app.use(session(sessionOptions));
 
-// Routes
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
@@ -60,7 +53,6 @@ Lab5(app);
 Hello(app);
 UserRoutes(app);
 
-// Start server
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });

ae72f93242548eace1f84077a684b09ed10ecedc
tcsimm <widet77@icloud.com>
2024-08-12 21:09:31 -0400

    push#

--- Users/routes.js
+++ Users/routes.js
@@ -48,9 +48,13 @@ const signout = (req, res) => {
 };
 
 const profile = (req, res) => {
+  console.log('Session ID:', req.sessionID);
+  console.log('Session Data:', req.session);
+  
   if (!req.session || !req.session.currentUser) {
     return res.status(401).json({ error: 'User not signed in' });
   }
+  
   res.json(req.session.currentUser);
 };
 
